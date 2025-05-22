aac4dfa845ebe9daf521b7092a622c8a79a2bf9c
yukiringo <141327996+yukiringo-apple@users.noreply.github.com>
2025-05-22 22:31:08 +0900

    add .gitignore .env

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 // server.js
 const express = require('express');
-const fetch = require('node-fetch'); // v3以降は ESM 対応必須、CommonJSなら v2を推奨
+const fetch = require('node-fetch');
 const app = express();
 const PORT = 3000;
 
-// CORS回避のためにAPIを中継
+// .envファイルの読み込み
+require('dotenv').config();
+const API_KEY = process.env.API_KEY;
+
+
+
+// 中継
 app.get('/api/search', async (req, res) => {
     const { lat, lng, range } = req.query;
-    const API_KEY = 'fd1c724889f2047b';
+
     const url = `https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${API_KEY}&lat=${lat}&lng=${lng}&range=${range}&format=json`;
 
     try {
