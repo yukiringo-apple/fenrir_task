209b16bc500441769878cf0390ded742c202faef
yukiringo <mone.kaigawa128@gmail.com>
2025-05-25 22:35:43 +0900

    add details.html details.js
    リストから詳細ページへの遷移

---
+++ js/results.js
@@ -0,0 +1,48 @@
+// result.js
+const results = JSON.parse(sessionStorage.getItem("searchResults"));
+
+console.log(results);
+
+
+function renderResults(results) {
+    const container = document.getElementById("results-container");
+    const isMobile = window.innerWidth < 768;
+
+    if (!results || results.length === 0) {
+        container.innerHTML = "<p>検索結果が見つかりませんでした。</p>";
+        return;
+    }
+
+    if (isMobile) {
+        results.forEach(shop => {
+
+        });
+    } else {
+        const listDiv = document.createElement("div");
+        listDiv.id = "shop-list";
+
+        results.forEach(shop => {
+            const item = document.createElement("div");
+            item.className = "shop-item";
+            item.innerHTML = `
+                <img class="img-shop-list-main-pc" src="${shop.photo.pc.l}">
+                <h4>${shop.name}</h4>
+                <p>${shop.address}</p>
+            `;
+
+            
+            item.addEventListener("click", () => {
+                sessionStorage.setItem("selectedShop", JSON.stringify(shop));
+                location.href = "details.html";
+            });
+
+            listDiv.appendChild(item);
+        });
+
+        container.appendChild(listDiv);
+    }
+}
+
+
+// レンダー
+renderResults(results);
--- js/search.js
+++ js/search.js
@@ -1,24 +1,18 @@
 // js/search.js
+const rangeInput = document.getElementById('rangeSelect');
+const labels = ["1", "2", "3", "4", "5"];
+let selectedRangeIndex = 3;
+
 function searchArea(latitude, longitude) {
-    // const selectElement = document.getElementById('selectArea');
-    // const selectedValue = selectElement.value;
-    // const selectedText = selectElement.options[selectElement.selectedIndex].text;
-
-
-    const rangeInput = document.getElementById('rangeSelect');
-    const selectedValueText = document.getElementById('selectedValue');
-    const labels = ["1", "2", "3", "4", "5"];
-    let selectedRangeIndex = 3;
 
     rangeInput.addEventListener('input', function () {
+        // event : rangeSliderを動かしたとき値を保存する
         selectedRangeIndex = parseInt(this.value);
-        // selectedValueText.textContent = labels[selectedRangeIndex];
     });
 
     const selectedValue = labels[selectedRangeIndex];
 
     console.log('選ばれた値:', selectedValue);
-    // console.log('表示テキスト:', selectedText);
 
     const url = `/api/search?lat=${latitude}&lng=${longitude}&range=${selectedValue}`;
 
@@ -31,14 +25,19 @@ function searchArea(latitude, longitude) {
         })
         .then(data => {
 
-            console.log("APIレスポンス全体:", data); // ← 追加
+            console.log("APIレスポンス全体:", data);
 
             if (!data.results || !data.results.shop) {
                 console.error("results.shop が存在しません", data);
                 return;
             }
+
             const results = data.results.shop;
             console.log(results);
+
+            sessionStorage.setItem("searchResults", JSON.stringify(data.results.shop));
+            window.location.href = "results.html";
+
         })
         .catch(error => {
             console.error("エラーが発生しました:", error);
